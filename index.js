aeb4096832fb87ec9c22d10d4379fb4112914013
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-31 17:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper'.slice(0, 1);
+const currentUser = 'Grace Hopper';
 
 
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
@@ -7,4 +7,4 @@ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
-const shortGreeting = `Welcome, ${currentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
