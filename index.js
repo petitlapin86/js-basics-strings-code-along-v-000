23d504593f61afde0ae35aa189bac1d504b31fa1
Paige Skinner <skinnerpa86@yahoo.com>
2017-10-31 17:47:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 
-const shortGreeting = `Welcome, ${currentUser} !`;
+const shortGreeting = `Welcome, ${currentUser}!`;
